219fad3fe38ac18ae9bab2ed4c3aec08691ea1d2
Dave Doolin <david.doolin@gmail.com>
2016-04-30 10:29:33 -0700

    working binary search tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,13 +1,33 @@
 require_relative './node'
 
 class Tree
-  attr_reader :root
+  attr_reader :root, :size
 
   def initialize node = nil
     @root = node ? node : Node.new
+    @size = 1
   end
 
   def add node
     @root.add node
-  end 
+    @size += 1
+  end
+
+  def collect
+    collector = []
+    get_values @root, collector
+    collector
+  end
+
+  def get_values node, collector
+    if node.left
+      get_values node.left, collector
+    end
+
+    collector << node.value
+
+    if node.right
+      get_values node.right, collector
+    end
+  end
 end
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -12,10 +12,72 @@ describe Tree do
     expect(tree.root).to eq node
   end
 
-  it 'adds a node to the tree with existing root' do
-    node = Node.new(1)
-    tree = Tree.new node
-    node2 = Node.new(2) 
-    tree.add node2
+  describe '.size' do
+    it 'adds a node to the tree with existing root' do
+      node = Node.new(1)
+      tree = Tree.new node
+      node2 = Node.new(2)
+      tree.add node2
+      expect(tree.size).to eq 2
+
+      node3 = Node.new(3)
+      tree.add node3
+      expect(tree.size).to eq 3
+    end
+  end
+
+  describe 'tree structure' do
+    it 'adds a node to the tree with existing root' do
+      node = Node.new(10)
+      tree = Tree.new node
+      tree.add Node.new(2)
+      expect(tree.size).to eq 2
+      expect(tree.root.left.value).to eq 2
+
+      tree.add Node.new(33)
+      expect(tree.size).to eq 3
+      expect(tree.root.right.value).to eq 33
+
+      tree.add Node.new(23)
+      expect(tree.size).to eq 4
+      expect(tree.root.right.left.value).to eq 23
+    end
+  end
+
+  describe 'collect node values' do
+    it 'collects node values for single node' do
+      node = Node.new(9)
+      tree = Tree.new node
+      expect(tree.size).to eq 1
+      expect(tree.collect).to eq [9]
+    end
+
+    it 'collects node values for left node only' do
+      node = Node.new(9)
+      tree = Tree.new node
+      tree.add Node.new(4)
+      expect(tree.size).to eq 2
+      expect(tree.collect).to eq [4, 9]
+    end
+
+    it 'collects node values for right node only' do
+      node = Node.new(9)
+      tree = Tree.new node
+      tree.add Node.new(14)
+      expect(tree.size).to eq 2
+      expect(tree.collect).to eq [9, 14]
+    end
+
+    it 'collects node values' do
+      node = Node.new(1)
+      tree = Tree.new node
+      tree.add Node.new(14)
+      tree.add Node.new(4)
+      tree.add Node.new(23)
+      tree.add Node.new(5)
+      tree.add Node.new(99)
+      expect(tree.size).to eq 6
+      expect(tree.collect).to eq [1, 4, 5, 14, 23, 99]
+    end
   end
 end
