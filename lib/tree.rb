fc6b27f9ae9223848a0342ea117fb54dea9e5f0a
Dave Doolin <david.doolin@gmail.com>
2016-04-30 05:03:21 -0700

    add from tree interface and rubocop

--- lib/tree.rb
+++ lib/tree.rb
@@ -8,14 +8,6 @@ class Tree
   end
 
   def add node
-    # This is the bogus. Let's make a simple binary tree
-    # first, without worrying about balance.
-    @root.left = node
-
-    return
-  
-    # if node.value < @root.value
-
-     
+    @root.add node
   end 
 end
