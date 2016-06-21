8e7ed257605f27de1b3d838c20d60d1bf1e4fc8c
Dave Doolin <david.doolin@gmail.com>
2016-06-21 15:14:22 -0700

    start of java implementation

---
+++ java/net/is/tree/bstree.java
@@ -0,0 +1,7 @@
+package net.is.tree;
+
+class BSTree {
+  public static void main(String [] args) {
+    System.out.println("From main...");
+  };
+};
---
+++ java/readme.md
@@ -0,0 +1,4 @@
+# Binary search tree in Java
+
+I haven't written Java in probably 10 years, probably more, so this is
+going to be very interesting!
