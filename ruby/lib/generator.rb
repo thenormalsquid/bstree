5a5ffee56a336ccbeed9c963835d2878ce8749a9
Dave Doolin <david.doolin@gmail.com>
2016-07-29 04:47:02 -0700

    more example generation

--- ruby/lib/generator.rb
+++ ruby/lib/generator.rb
@@ -15,4 +15,36 @@ class Generator
   def self.tree2
     Generator.new.build [11, 7]
   end
+
+  def self.tree3
+    Generator.new.build [11, 7, 13]
+  end
+
+  def self.tree4
+    Generator.new.build [11, 7, 5]
+  end
+
+  def self.tree5
+    Generator.new.build [5, 7, 11]
+  end
+
+  def self.tree6
+    Generator.new.build [11, 5, 3, 7, 13]
+  end
+
+  def self.tree7
+    Generator.new.build [5, 3, 11, 7, 13]
+  end
+
+  def self.tree8
+    Generator.new.build [11, 5, 3, 7, 17, 13, 19]
+  end
+
+  def self.tree9
+    Generator.new.build [19, 23, 13, 17, 11, 5, 7]
+  end
+
+  def self.tree10
+    Generator.new.build [11, 5, 3, 19, 17, 29, 23, 41, 37, 53]
+  end
 end
--- ruby/spec/generator_spec.rb
+++ ruby/spec/generator_spec.rb
@@ -2,20 +2,12 @@ require_relative './spec_helper'
 require_relative '../lib/generator'
 
 describe Generator do
-  it 'instantiates a binary search tree generator' do
-    expect(Generator.new.build([1])).not_to be nil
-  end
-
-  it 'builds a one node tree' do
-    tree = Generator.new.build([11])
-    expect(tree.size).to eq 1
-  end
-
   it 'builds tree1' do
     tree1 = Generator.tree1
     expect(tree1.size).to eq 1
     expect(tree1.bst?).to be true
     expect(tree1.full?).to be true
+    # expect(tree.pathological??).to be false
   end
 
   it 'builds tree2' do
@@ -23,5 +15,70 @@ describe Generator do
     expect(tree2.size).to eq 2
     expect(tree2.bst?).to be true
     expect(tree2.full?).to be nil
+    # expect(tree.pathological??).to be false
+  end
+
+  it 'builds tree3' do
+    tree3 = Generator.tree3
+    expect(tree3.size).to eq 3
+    expect(tree3.bst?).to be true
+    expect(tree3.full?).to be true
+    # expect(tree.pathological??).to be false
+  end
+
+  it 'builds tree4' do
+    tree = Generator.tree4
+    expect(tree.size).to eq 3
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be nil
+    # expect(tree.pathological??).to be true
+  end
+
+  it 'builds tree5' do
+    tree = Generator.tree5
+    expect(tree.size).to eq 3
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be nil
+    # expect(tree.pathological??).to be true
+  end
+
+  it 'builds tree6' do
+    tree = Generator.tree6
+    expect(tree.size).to eq 5
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be true
+    # expect(tree.pathological??).to be true
+  end
+
+  it 'builds tree7' do
+    tree = Generator.tree7
+    expect(tree.size).to eq 5
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be true
+    # expect(tree.pathological??).to be true
+  end
+
+  it 'builds tree8' do
+    tree = Generator.tree8
+    expect(tree.size).to eq 7
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be true
+    # expect(tree.pathological??).to be true
+  end
+
+  it 'builds tree9' do
+    tree = Generator.tree9
+    expect(tree.size).to eq 7
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be nil
+    # expect(tree.pathological??).to be true
+  end
+
+  it 'builds tree10' do
+    tree = Generator.tree10
+    expect(tree.size).to eq 10
+    expect(tree.bst?).to be true
+    expect(tree.full?).to be nil
+    # expect(tree.pathological??).to be true
   end
 end
