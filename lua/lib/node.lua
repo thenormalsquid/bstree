933fa40f84800b94883cb89fcdca334c81df87cd
Dave Doolin <david.doolin@gmail.com>
2016-07-30 06:55:41 -0700

    first cut at insert in lua

--- lua/lib/node.lua
+++ lua/lib/node.lua
@@ -1,9 +1,24 @@
-module 'node'
+local print = print
 
-function new(key)
-  return {
+local Node = {}
+
+function Node:new(key)
+  local node = {
     key = key,
     left = nil,
     right = nil
   }
+  setmetatable(node, self)
+  self.__index = self
+  return node
 end
+
+function Node:insert(n)
+  if n.key < self.key then
+    self.left = n
+  else
+    self.right = n
+  end
+end
+
+return Node
--- lua/spec/node_spec.lua
+++ lua/spec/node_spec.lua
@@ -1,6 +1,6 @@
 require 'busted.runner'()
 
-require 'lib/node'
+node = require 'lib/node'
 
 describe("instantiating nodes", function()
   it("instantiates a node", function()
@@ -9,6 +9,17 @@ describe("instantiating nodes", function()
       left = nil,
       right = nil
     }
-    assert.are.same(table, node.new(11))
+    assert.are.same(table, node:new(11))
   end)
 end)
+
+describe("insert nodes", function()
+  it("inserts a single node", function()
+    root = node:new(11)
+    right = node:new(13)
+    root:insert(right)
+    assert.are.same(root.right, right)
+  end)
+end)
+
+
