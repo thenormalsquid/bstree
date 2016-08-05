3dbd1b1e3863b793d81bc99f3a0b5579905e984f
Dave Doolin <david.doolin@gmail.com>
2016-08-05 05:57:36 -0700

    more avl

--- ruby/lib/avl_node.rb
+++ ruby/lib/avl_node.rb
@@ -12,21 +12,37 @@ class AvlNode < Node
   def add node
     super
     node < self ? @weight -= 1 : @weight += 1
+
     # puts weight
+    if weight >= 2
+      # rotate_cw
+    end
     # super will just add node.
     # check the current weights here.
     # the hard thing is understanding that the root
     # node may have to change to ensure balance.
   end
 
-  def rotate_ccw
+  def insert node, parent = nil
+    node < self ? insert_left(node, self) : insert_right(node, self)
+  end
+
+  def insert_left node, parent
+    @left.nil? ? @left = node : @left.insert(node, self)
+  end
+
+  def insert_right node, parent
+    @right.nil? ? @right = node : @right.insert(node, self)
+  end
+
+  def rotate_cw
     pivot = self.left
     swinger = pivot.right
     self.left = swinger
     pivot.right = self
   end
 
-  def rotate_cw
+  def rotate_ccw
     pivot = self.right
     swinger = pivot.left
     self.right = swinger
--- ruby/spec/avl_node_spec.rb
+++ ruby/spec/avl_node_spec.rb
@@ -20,36 +20,61 @@ describe AvlNode do
   end
 
   describe 'rotations' do
-    it 'rotates counterclockwise' do
+    it 'rotates counterclockwise with 3 nodes right' do
       root = AvlNode.new 17
       n23 = AvlNode.new 23
-      root.add n23
+      n29 = AvlNode.new 29
+      root.insert n23
+      root.insert n29
+
+      root.rotate_ccw
+      expect(n23.left).to eq root
+      expect(n23.right).to eq n29
+      expect(n23.size).to eq 3
+    end
+
+    it 'rotates clockwise with 3 nodes left' do
+      root = AvlNode.new 11
+      n5 = AvlNode.new 5
+      n3 = AvlNode.new 3
+      root.insert n5
+      root.insert n3 
+      root.rotate_cw
+      expect(n5.right).to eq root
+      expect(n5.left).to eq n3
+      expect(n5.size).to eq 3
+    end
+
+    it 'rotates clockwise' do
+      root = AvlNode.new 17
+      n23 = AvlNode.new 23
+      root.insert n23
 
       n11 = AvlNode.new 11
       n13 = AvlNode.new 13
       n7 = AvlNode.new 7
-      root.add n11
-      root.add n13
-      root.add n7
+      root.insert n11
+      root.insert n13
+      root.insert n7
 
-      root.rotate_ccw
+      root.rotate_cw
       expect(n11.right).to eq root
       expect(root.left).to eq n13
       expect(n11.size).to eq 5
     end
 
-    it 'rotates clockwise' do
+    it 'rotates counterclockwise' do
       root = AvlNode.new 11
       n7 = AvlNode.new 7
-      root.add n7
+      root.insert n7
       n17 = AvlNode.new 17
       n13 = AvlNode.new 13
       n23 = AvlNode.new 23
-      root.add n17
-      root.add n13
-      root.add n23
+      root.insert n17
+      root.insert n13
+      root.insert n23
 
-      root.rotate_cw
+      root.rotate_ccw
       expect(n17.left).to eq root
       expect(root.right).to eq n13
       expect(n17.size).to eq 5
@@ -77,18 +102,23 @@ describe AvlNode do
       #
       # expect(n2).to receive(:rotate_ccw).with("correct argument")
       describe 'only right children' do
-        it 'makes a long right list' do
+        xit 'makes a long right list' do
           n2.add n3
           n2.add n5
-          n2.add n7
-          n2.add n11
-          n2.add n13
-          n2.add n17
-          n2.add n19
-          n2.add n23
-          n2.add n29
-          expect(n2.height).to eq 9
-          expect(n29.height).to eq 0
+          expect(n2).to receive(:rotate_cw) # .with("correct argument")
+          # expect(n2.height).to eq 1
+          # expect(n3.height).to eq 0
+          # expect(n3.height).to eq 0
+          # expect(n2.pathological?).to be false
+
+          # n2.add n11
+          # n2.add n13
+          # n2.add n17
+          # n2.add n19
+          # n2.add n23
+          # n2.add n29
+          # expect(n2.height).to eq 9
+          # expect(n29.height).to eq 0
           # expect(n2.pathological?).to be true
           # expect(n29.pathological?).to be false
           # expect(n23.pathological?).to be false
@@ -98,7 +128,7 @@ describe AvlNode do
       end
 
       describe 'only left children' do
-        it 'makes a long left list' do
+        xit 'makes a long left list' do
           n29.add n23
           n29.add n19
           n29.add n17
