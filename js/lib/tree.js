927d6dee1356081d306fc551fb1bd4944de06866
Dave Doolin <david.doolin@gmail.com>
2016-08-20 07:17:08 -0700

    js tree insert and is_empty

--- js/lib/tree.js
+++ js/lib/tree.js
@@ -1,13 +1,17 @@
-var Tree = function () {};
+var Tree = function () {
+  this.root = null;
+};
 
 Tree.prototype.log = function (msg) {
   console.log(msg);
 };
 
-Tree.prototype.new = function () {
-  return {
-    root: null
-  };
+Tree.prototype.insert = function (n) {
+  this.root === null ? this.root = n : this.root.insert(n);
 }
 
-exports.Tree = new Tree();
+Tree.prototype.is_empty = function () {
+  return this.root === null;
+};
+
+exports.Tree = Tree;
--- js/spec/tree_spec.js
+++ js/spec/tree_spec.js
@@ -5,62 +5,43 @@ var assert = require('assert');
 describe('Tree', function() {
   describe('instantiation', function() {
     it('creates a Tree', function() {
-      var t = tree.new();
-      //console.log(t);
+      var t = new tree();
       assert.equal(t.root, null);
     });
   });
 
   describe('insertion', function() {
-    xit('insert root node into emptry tree', function() {
-      var t = tree.new();
-      var root = node.new(13);
+    it('insert root node into empty tree', function() {
+      var t = new tree();
+      var root = new node(13);
       t.insert(root);
       assert.equal(t.root, root);
     });
 
-    xit('insert left and right nodes', function() {
-      var t = tree.new();
-      var root = node.new(13);
+    it('insert left and right nodes', function() {
+      var t = new tree();
+      var root = new node(13);
       t.insert(root);
-      var n5 = node.new(5);
-      var n19 = node.new(19);
+      var n5 = new node(5);
+      var n19 = new node(19);
       t.insert(n5);
       t.insert(n19);
-      assert.equal(t.root, root);
-    });
-
-    xit('insert first 10 primes', function() {
-      var t = tree.new();
-      var root = node.new(13);
-      t.insert(root);
-      var n7 = node.new(7);
-      var n5 = node.new(5);
-      var n3 = node.new(3);
-      var n2 = node.new(2);
-      var n11 = node.new(11);
-      var n17 = node.new(17);
-      var n19 = node.new(19);
-      var n23 = node.new(23);
-      var n29 = node.new(29);
-      t.insert(n5);
-      t.insert(n19);
-      //t.insert(n19).insert(17).insert(n23).insert(n11).insert(n7).insert(n3).insert(n2).insert(n29);
-      assert.equal(t.root, root);
+      assert.equal(t.root.left, n5);
+      assert.equal(t.root.right, n19);
     });
   });
 
   describe('is_empty', function() {
-    xit('insert root node into emptry tree', function() {
-      var t = tree.new();
-      assert.equal(t.is_empty, true);
+    it('insert root node into empty tree', function() {
+      var t = new tree();
+      assert.equal(t.is_empty(), true);
     });
 
-    xit('insert root node into emptry tree', function() {
-      var t = tree.new();
-      var root = node.new(13);
+    it('insert root node into empty tree', function() {
+      var t = new tree();
+      var root = new node(13);
       t.insert(root);
-      assert.equal(t.is_empty, false);
+      assert.equal(t.is_empty(), false);
     });
   });
 
