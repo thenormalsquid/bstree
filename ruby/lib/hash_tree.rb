18c9b0d2c8556ac7c19fa53d4917a801ee53f927
Dave Doolin <david.doolin@gmail.com>
2016-11-27 04:28:15 -0800

    hash tree - super dumb document insertion
    Get some documents into the tree and hash them at
    will.
    
    This commit looks really stupid because it is. Mostly,
    it's a way to plow through my current log jam on hash
    trees. Not feeling the love for it at the moment, so
    just writing anything is a step forward.

--- ruby/lib/hash_tree.rb
+++ ruby/lib/hash_tree.rb
@@ -1,2 +1,24 @@
+require 'digest'
+
 class HashTree
+  class Node
+    attr_accessor :key, :left, :right, :parent
+  end
+
+  attr_accessor :root, :documents, :hashes
+
+  def initialize
+    @documents = []
+    @hashes = []
+  end
+
+  def insert document
+    @documents << document
+  end
+
+  def rehash
+    @documents.each do |document|
+      @hashes << Digest::SHA256.hexdigest(document)
+    end
+  end
 end
--- ruby/spec/hash_tree_spec.rb
+++ ruby/spec/hash_tree_spec.rb
@@ -2,7 +2,26 @@
 require_relative '../lib/hash_tree.rb'
 
 describe HashTree do
+  describe HashTree::Node do
+    it 'instantiates' do
+      expect(described_class).not_to be nil
+    end
+  end
+
   it 'instantiates' do
     expect(HashTree.new).not_to be nil
+  end
+
+  describe 'insert' do
+    it 'hashes inserted documents automatically' do
+      ht = HashTree.new
+      doc1 = ""
+      doc2 = "1"
+      ht.insert doc1
+      ht.insert doc2
+      ht.rehash
+      expect(ht.hashes[0]).to eq 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+      expect(ht.hashes[1]).to eq '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'
+    end
   end
 end
