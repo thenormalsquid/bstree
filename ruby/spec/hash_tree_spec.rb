d56942376b7ba2553373798f02cc58cb94b18e46
Dave Doolin <david.doolin@gmail.com>
2016-12-29 06:57:19 -0800

    hash tree - rubocop

--- ruby/spec/hash_tree_spec.rb
+++ ruby/spec/hash_tree_spec.rb
@@ -20,8 +20,10 @@ describe HashTree do
       ht.insert doc1
       ht.insert doc2
       ht.rehash
-      expect(ht.hashes[0]).to eq 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
-      expect(ht.hashes[1]).to eq '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'
+      h1 = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+      h2 = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'
+      expect(ht.hashes[0]).to eq h1
+      expect(ht.hashes[1]).to eq h2
     end
   end
 end
