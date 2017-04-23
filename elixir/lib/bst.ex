3be78818f8708c2534d3f0d7097ab71281875b4c
Dave Doolin <david.doolin@gmail.com>
2017-04-22 19:54:01 -0700

    elixir - first commit, stub project with mix

---
+++ elixir/lib/bst.ex
@@ -0,0 +1,18 @@
+defmodule Bst do
+  @moduledoc """
+  Documentation for Bst.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bst.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/test/bst_test.exs
@@ -0,0 +1,8 @@
+defmodule BstTest do
+  use ExUnit.Case
+  doctest Bst
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
