410e7c5231285086e6490710ecd2f77e498a8254
dave doolin <david.doolin@gmail.com>
2017-05-11 19:10:43 -0700

    stern-brocot - integers >= 0 work
    This turned out to be a bit easier than I
    thought it would be, for at least the
    simple (trivial) iterative technique. Hopefully
    I got it right and it will work for rational
    numbers, because that's the point.
    
    Good day over all. This is strictly recreational
    coding at this point, being 7 pm. I think I will
    attempt to start ramping down from caffeine
    tomorrow. Will be a brutal weekend. I'll drink
    some tea with honey may, and get some heavy
    cream from the grocery store.

--- ruby/lib/stern_brocot.rb
+++ ruby/lib/stern_brocot.rb
@@ -2,27 +2,64 @@ class SternBrocot
   MAX = 2 ** (1.size * 8 - 2) - 1
 
   def self.rationalize number
+    return [0, 1] if number.to_f == 0.0
+
     # q is a particular node with a rational number
     # expressed in the form n/d.
-    q = 0
+    q = number
 
     a = 0
     b = 1
     c = 1
-    d = MAX
+    d = 0 # 1/MAX
 
     # loop here
     l = a/b
-    h = c/d
-    m = (a+b)/(c+ d)
+    # h = c/d
+    h = d.zero? ?  MAX : c/d
+    mn = a + b
+    md = c + d
+    m = mn / md.to_f
 
-    return [a+b, c+d] if q == m
+    # first, ensure we're getting 1/1 back.
+    # return [mn.to_i, md.to_i] if m == q
 
-    if m < q
-      l = m
-    else
-      h = m
+    iteration = 1
+
+    # binding.pry if q.zero?
+
+    while true
+      # before trying to iterate (or recurse), let's do q < 1, then q > 1
+
+      return [mn.to_i, md.to_i] if m == q
+
+      # binding.pry
+
+      if m < q # on the first iteration, this means the q >= 1
+        a = mn
+        b = md
+        l = m
+        # binding.pry
+      else
+        c = mn
+        d = md
+        h = m
+      end
+
+      mn = a + b
+      md = c + d
+      m = mn / md.to_f
+
+      return [mn.to_i, md.to_i] if m == q
+
+      iteration += 1
+      puts md.to_i if md.to_i.modulo(5_000_000).zero?
+      break if iteration > 2001 # MAX - 1
+
     end
+
+    return [mn.to_i, md.to_i]
+
     # also, return if past a certain threshold in precision
     # return [a+b, c+d] if Math.abs(q - number) < 0.0005
     # increment a, b, c, d
--- ruby/spec/stern_brocot_spec.rb
+++ ruby/spec/stern_brocot_spec.rb
@@ -7,20 +7,43 @@ RSpec.describe SternBrocot do
   end
 
   describe '.rationalize' do
-    context 'integers' do
-      example '0'
-      example '1'
-      example '2'
-      example '1246'
+    context 'integers >= 0' do
+      example '0' do
+        actual = SternBrocot.rationalize 0
+        expect(actual).to eq [0, 1]
+      end
+
+      example '1' do
+        actual = SternBrocot.rationalize 1
+        expect(actual).to eq [1, 1]
+      end
+
+      example '2' do
+        actual = SternBrocot.rationalize 2
+        expect(actual).to eq [2, 1]
+      end
+
+      example '1246' do
+        actual = SternBrocot.rationalize 1246
+        expect(actual).to eq [1246, 1]
+      end
     end
 
-    context 'rational numbers' do
+    context 'rational numbers <= 1.0' do
       example '0.0'
       example '1.0'
       example '0.5'
       example '1.5'
       example '0.3'
       example '0.33'
+    end
+
+    context 'rational numbers > 1.0' do
+      example '1.1'
+      example '3.1'
+      example '5.2'
+      example '9.9'
+      example '123.123'
     end
 
     context 'irrational numbers' do
