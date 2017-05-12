d681f560245aadc649b0b5609e422d059dee67c2
dave doolin <david.doolin@gmail.com>
2017-05-12 11:56:24 -0700

    stern-brocot - rational numbers <= 1
    This is partially working, committed to keep
    track of where the implementation needs to be
    fixed. Whatever the problem is, it's almost
    surely very simple.
    
    Medium coffee today, on the start of the ramp down.
    Will get a small tomorrow. This is going to work, as
    I'm between 1/3 and 1/2 left on the medium and it's
    noon already.
    
    Apparently, El Jefe Naranja is losing his marbles
    over the Comey thing. Would be very entertaining
    except for the nuclear weapons thingy.

--- ruby/lib/stern_brocot.rb
+++ ruby/lib/stern_brocot.rb
@@ -1,69 +1,56 @@
 class SternBrocot
   MAX = 2 ** (1.size * 8 - 2) - 1
+  TOL = 0.001
 
   def self.rationalize number
     return [0, 1] if number.to_f == 0.0
 
-    # q is a particular node with a rational number
-    # expressed in the form n/d.
     q = number
 
     a = 0
     b = 1
     c = 1
-    d = 0 # 1/MAX
+    d = 0
 
-    # loop here
     l = a/b
-    # h = c/d
-    h = d.zero? ?  MAX : c/d
+    h = d.zero? ? MAX : c/d
     mn = a + b
     md = c + d
     m = mn / md.to_f
 
-    # first, ensure we're getting 1/1 back.
-    # return [mn.to_i, md.to_i] if m == q
-
-    iteration = 1
-
-    # binding.pry if q.zero?
+    iteration = 1 # ejection handle
 
     while true
-      # before trying to iterate (or recurse), let's do q < 1, then q > 1
-
       return [mn.to_i, md.to_i] if m == q
 
       # binding.pry
 
-      if m < q # on the first iteration, this means the q >= 1
+      if m < q
         a = mn
         b = md
-        l = m
-        # binding.pry
-      else
+        # l = m
+      else # m > q
         c = mn
         d = md
-        h = m
+        # h = m
       end
 
       mn = a + b
       md = c + d
       m = mn / md.to_f
 
-      return [mn.to_i, md.to_i] if m == q
+      # return [mn.to_i, md.to_i] if m == q
+      return [mn.to_i, md.to_i] if (m - q).abs < TOL
 
       iteration += 1
-      puts md.to_i if md.to_i.modulo(5_000_000).zero?
-      break if iteration > 2001 # MAX - 1
+      # puts md.to_i if md.to_i.modulo(10).zero?
+      break if iteration > 15
 
     end
 
-    return [mn.to_i, md.to_i]
-
     # also, return if past a certain threshold in precision
     # return [a+b, c+d] if Math.abs(q - number) < 0.0005
-    # increment a, b, c, d
-    # end loop here
+    [mn.to_i, md.to_i]
 
 =begin
     Initialize two values L and H to 0/1 and 1/0, respectively.
--- ruby/spec/stern_brocot_spec.rb
+++ ruby/spec/stern_brocot_spec.rb
@@ -8,44 +8,78 @@ RSpec.describe SternBrocot do
 
   describe '.rationalize' do
     context 'integers >= 0' do
-      example '0' do
+      example '0 yields 0/1' do
         actual = SternBrocot.rationalize 0
         expect(actual).to eq [0, 1]
       end
 
-      example '1' do
+      example '1 yields 1/1' do
         actual = SternBrocot.rationalize 1
         expect(actual).to eq [1, 1]
       end
 
-      example '2' do
+      example '2 yields 2/1' do
         actual = SternBrocot.rationalize 2
         expect(actual).to eq [2, 1]
       end
 
-      example '1246' do
+      example '1246 yields 1246/1' do
         actual = SternBrocot.rationalize 1246
         expect(actual).to eq [1246, 1]
       end
     end
 
     context 'rational numbers <= 1.0' do
-      example '0.0'
-      example '1.0'
-      example '0.5'
-      example '1.5'
-      example '0.3'
-      example '0.33'
+      example '0.0 yields 0/1' do
+        actual = SternBrocot.rationalize 0.0
+        expect(actual).to eq [0, 1]
+      end
+
+      example '1.0 yields 1/1' do
+        actual = SternBrocot.rationalize 1.0
+        expect(actual).to eq [1, 1]
+      end
+
+      example '0.5 yields 1/2' do
+        actual = SternBrocot.rationalize 0.5
+        expect(actual).to eq [1, 2]
+      end
+
+      example '0.1 yields 1/10' do
+        actual = SternBrocot.rationalize 0.1
+        expect(actual).to eq [1, 10]
+      end
+
+      xexample '0.15 yields 15/100' do
+        binding.pry
+        actual = SternBrocot.rationalize 0.15
+        expect(actual).to eq [15, 100]
+      end
+
+      xexample '0.3 yields 3/10' do
+        actual = SternBrocot.rationalize 0.3
+        expect(actual).to eq [3, 10]
+      end
+
+      xexample '0.333 yields 333/1000' do
+        actual = SternBrocot.rationalize 0.333
+        expect(actual).to eq [333, 1000]
+      end
     end
 
     context 'rational numbers > 1.0' do
-      example '1.1'
+      xexample '1.1 yields 11/10' do
+        actual = SternBrocot.rationalize 1.1
+        expect(actual).to eq [11, 10]
+      end
+
       example '3.1'
       example '5.2'
       example '9.9'
       example '123.123'
     end
 
+=begin
     context 'irrational numbers' do
       context 'pi' do
         example 'to 1 decimal place'
@@ -63,5 +97,6 @@ RSpec.describe SternBrocot do
         example 'to 5 decimal places'
       end
     end
+=end
   end
 end
