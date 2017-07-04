16effae5016f1518743e1efb12bc6c9e193629fd
dave doolin <david.doolin@gmail.com>
2017-07-04 09:08:45 -0700

    avl: too many rotations on inorder insertion
    The inorder insertions induce pathological rotations
    skewing the tree out of balance to left, that is, left
    heavy. Not sure why quite yet. Will require some major
    digging into the rebalancing logic.

--- ruby/spec/avl_fun_and_games_spec.rb
+++ ruby/spec/avl_fun_and_games_spec.rb
@@ -52,6 +52,70 @@ describe AvlTree do
       tree.insert n6
       expected = [4, 2, 1, 3, 5, 6]
       expect(tree.preorder_keys).to eq expected
+      expect(tree.root).to eq n4
+      expect(tree.root.balance_factor).to eq 0
+      expect(n1.balance_factor).to eq 0
+      expect(n2.balance_factor).to eq 0
+      expect(n3.balance_factor).to eq 0
+      expect(n4.balance_factor).to eq 0 # root!
+      expect(n5.balance_factor).to eq 1
+      expect(n6.balance_factor).to eq 0
+
+      # "perfect" tree
+      tree.insert n7
+      expect(tree.height).to eq 2
+      expect(tree.size).to eq 7
+      expected = [4, 2, 1, 3, 6, 5, 7]
+      actual = tree.preorder_keys
+      expect(actual).to eq expected
+
+      tree.insert n8
+      expect(tree.height).to eq 3
+      expect(n7.right).to eq n8
+
+      tree.insert n9
+      expect(tree.height).to eq 3
+      actual = tree.preorder_keys
+      expected =  [6, 4, 2, 1, 3, 5, 8, 7, 9]
+      expect(actual).to eq expected
+
+
+      tree.insert n10
+      expect(tree.height).to eq 3
+      actual = tree.preorder_keys
+      expected =  [6, 4, 2, 1, 3, 5, 8, 7, 9, 10]
+      expect(actual).to eq expected
+
+
+      tree.insert n11
+      # Failing here, the tree rotates too far to the left
+      # making the left side out of balance. This is going to
+      # require working through both the examples on the web
+      # and building examples by hand to figure it out. The
+      # end goal is a perfect tree with 15 nodes, height 3.
+      # I'm pretty sure this is a bookkeeping issue with the
+      # rotations, at least, I'm sure hoping so.
+      #
+      # expect(tree.height).to eq 3
+      actual = tree.preorder_keys
+      puts "actual: #{actual}"
+
+
+      # This is almost surely not working correctly.
+      # I have a hunch that the other rotation needs to
+      # be fixed so that it will balance properly. Then
+      # again, all the nodes are going in on the far right
+      # so it might correct.
+      tree.insert n12
+      # expect(tree.height).to eq 3
+      tree.insert n13
+      # expect(tree.height).to eq 3
+      tree.insert n14
+      # expect(tree.height).to eq 3
+      tree.insert n15
+      # expect(tree.height).to eq 3
+      actual = tree.preorder_keys
+      puts "actual: #{actual}"
     end
   end
 end
